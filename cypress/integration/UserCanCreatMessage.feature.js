1328a29c8fc2fadde7a3a38b76def803cbf7cb28
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-19 14:58:00 +0200

    User can interact with the (press me) button

--- cypress/integration/UserCanCreatMessage.feature.js
+++ cypress/integration/UserCanCreatMessage.feature.js
@@ -1,7 +1,12 @@
 
 describe('user can create message', () => {
 	it('test', () => {
-        cy.visit('http://localhost:3001')
-        cy.get('#quote').click()
+        cy.visit('http://localhost:3001') 
+
+    })
+        
+        it('can click on button', () => {
+            cy.get('#quote').click()
+        
 	})
 })
